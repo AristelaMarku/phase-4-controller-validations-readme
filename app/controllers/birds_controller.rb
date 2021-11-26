35c6cfd657a85a19ad04bfb88c53085d547f032b
Aristela Marku <aristela.marku@gmail.com>
2021-11-26 17:47:38 +0100

    commit tree eebaf1f31e5eeca6ad38da7c4cab806de7f33599

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,57 +1,105 @@
 class BirdsController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-  # GET /birds
-  def index
-    birds = Bird.all
-    render json: birds
-  end
+  # # GET /birds
+  # def index
+  #   birds = Bird.all
+  #   render json: birds
+  # end
 
-  # POST /birds
-  def create
-    bird = Bird.create(bird_params)
-    render json: bird, status: :created
+  # # POST /birds
+  # def create
+  #   bird = Bird.create!(bird_params)
+  #   render json: bird, status: :created
 
-    end
+  #   end
 
-  # GET /birds/:id
-  def show
-    bird = find_bird
-    render json: bird
-  end
+  # # GET /birds/:id
+  # def show
+  #   bird = find_bird
+  #   render json: bird
+  # end
 
-  # PATCH /birds/:id
-  def update
-    bird = find_bird
-    bird.update!(bird_params)
-    render json: bird
+  # # PATCH /birds/:id
+  # def update
+  #   bird = find_bird
+  #   bird.update!(bird_params)
+  #   render json: bird
  
+  # end
+
+  # # DELETE /birds/:id
+  # def destroy
+  #   bird = find_bird
+  #   bird.destroy
+  #   head :no_content
+  # end
+
+  # private
+
+  # def find_bird
+  #   Bird.find(params[:id])
+  # end
+
+  # def bird_params
+  #   params.permit(:name, :species, :likes)
+  # end
+
+  # def render_not_found_response
+  #   render json: { error: "Bird not found" }, status: :not_found
+  # end
+
+  # def render_unprocessable_entity_response(invalid)
+  #   render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  # end
+
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def index
+  birds=Bird.all
+  render json: birds
   end
 
-  # DELETE /birds/:id
+  def show
+  bird=find_bird
+  render json: bird
+  end
+
+  def create
+  bird=Bird.create!(bird_params)
+  render json: bird, status: :created
+  end
+
+  def update
+  bird=find_bird
+  bird.update!(bird_params)
+  render json: bird
+  end
+
   def destroy
-    bird = find_bird
-    bird.destroy
-    head :no_content
+  bird=find_bird
+  bird.destroy
+  head :no_content
   end
 
   private
 
   def find_bird
-    Bird.find(params[:id])
+   Bird.find(params[:id])
   end
 
   def bird_params
-    params.permit(:name, :species, :likes)
+  params.permit(:name, :species)
   end
 
   def render_not_found_response
-    render json: { error: "Bird not found" }, status: :not_found
+  render json: {error: "Not found"}, status: :not_found
   end
 
   def render_unprocessable_entity_response(invalid)
+   
     render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
-
 end
